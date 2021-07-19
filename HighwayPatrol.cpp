fe0cc262c2265a3a45e78d7635e6b18979c15d60
Stokely Diamantis <stokelydiamantis@gmail.com>
2021-07-19 03:51:27 +0000

    added vehicle type to std::cout of HighWayPatrol

--- HighwayPatrol.cpp
+++ HighwayPatrol.cpp
@@ -1,5 +1,9 @@
 #include "HighwayPatrol.h"
 #include "Highway.h"
+#include "Car.h"
+#include "Motorcycle.h"
+#include "SemiTruck.h"
+
 #include <iostream>
 
 HighwayPatrol::HighwayPatrol() : Vehicle("HighwayPatrol")
@@ -28,8 +32,22 @@ void HighwayPatrol::pullOver( Vehicle* v, bool willArrest, Highway* h )
     std::cout << name << ": vehicle is traveling " << v->speed - h->speedLimit << " miles per hour over the speed limit" << std::endl;
     if( willArrest )
     {
+        std::string vehicleType;
+        if( auto* ctype = dynamic_cast<Car*>(v) )
+        {
+            vehicleType = "CAR";
+        }
+        else if( auto* mtype = dynamic_cast<Motorcycle*>(v) )
+        {
+            vehicleType = "MOTORCYCLE";
+        }
+        else if( auto* stType = dynamic_cast<SemiTruck*>(v) )
+        {
+            vehicleType = "SEMITRUCK";
+        }
+
         //print the vehicle type in this std::cout between "THE [" and "] PULL". 
-        std::cout << name << ": YOU IN THE [ " << " ] PULL OVER AND SHOW YOUR HANDS" << std::endl;
+        std::cout << name << ": YOU IN THE [ " << vehicleType << " ] PULL OVER AND SHOW YOUR HANDS" << std::endl;
         std::cout << "EVERYONE ELSE, SLOW DOWN!! \n\n\n";
         h->removeVehicle(v);
     }
